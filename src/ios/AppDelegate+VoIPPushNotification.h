7ebe7e45d5af90f0caa86d4297c1e6804ced78c2
Richard Lewin <>
2018-10-17 15:15:25 +0100

    Altered AppDelegate code (iOS).

--- src/ios/AppDelegate+VoIPPushNotification.h
+++ src/ios/AppDelegate+VoIPPushNotification.h
@@ -2,6 +2,8 @@
 #import <Availability.h>
 #import <PushKit/PushKit.h>
 
+UIBackgroundTaskIdentifier bgTask;
+
 @interface AppDelegate (VoIPPushNotification)
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions;
--- src/ios/AppDelegate+VoIPPushNotification.m
+++ src/ios/AppDelegate+VoIPPushNotification.m
@@ -4,25 +4,21 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     UIApplication* app = [UIApplication sharedApplication];
-    self.bgTask = [app beginBackgroundTaskWithExpirationHandler:^{
-        [app endBackgroundTask:self.bgTask];
-        self.bgTask = UIBackgroundTaskInvalid;
+    bgTask = [app beginBackgroundTaskWithExpirationHandler:^{
+        [app endBackgroundTask:bgTask];
+        bgTask = UIBackgroundTaskInvalid;
     }];
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        while (true) {
-            ;
-        }
-    });
-    PKPushRegistry *pushRegistry =
-    [[PKPushRegistry alloc] initWithQueue:dispatch_get_main_queue()];
+    PKPushRegistry *pushRegistry = [[PKPushRegistry alloc] initWithQueue:dispatch_get_main_queue()];
     pushRegistry.delegate = self;
     pushRegistry.desiredPushTypes = [NSSet setWithObject:PKPushTypeVoIP];
-    return YES;
+    [super application:application didFinishLaunchingWithOptions:launchOptions];
 };
 
 - (void)pushRegistry:(PKPushRegistry *)registry didUpdatePushCredentials:(PKPushCredentials *)credentials forType:(NSString *)type {
     UIApplication* app = [UIApplication sharedApplication];
-    [app endBackgroundTask:self.bgTask];
+    [app endBackgroundTask:bgTask];
 }
+
+@dynamic bgTask;
 
 @end
