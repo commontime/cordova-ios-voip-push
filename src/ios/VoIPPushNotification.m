8e632ee36ee3661d2f86a5c12105516f8758d21b
Richard Lewin <richardlewin@Richards-MacBook-Pro-2.local>
2019-03-14 15:23:29 +0000

    Local notitfication fireing timing alteration

--- src/ios/VoIPPushNotification.m
+++ src/ios/VoIPPushNotification.m
@@ -95,7 +95,7 @@
 
     if (!foregrounded) {
         UILocalNotification *notification = [[UILocalNotification alloc] init];
-        notification.fireDate = [NSDate dateWithTimeIntervalSinceNow:7];
+        notification.fireDate = [NSDate dateWithTimeIntervalSinceNow:5];
         notification.alertBody = @"New Message Received";
         notification.timeZone = [NSTimeZone defaultTimeZone];
         [[UIApplication sharedApplication] scheduleLocalNotification:notification];  
