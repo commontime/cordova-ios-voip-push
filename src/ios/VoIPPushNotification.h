bdffb695938a8aa28d3cc80e12a317d84ccbf38f
Leon Revill <leon@revillwebdesign.com>
2019-07-03 13:03:11 +0100

    Add additional audio player instances

--- src/ios/VoIPPushNotification.h
+++ src/ios/VoIPPushNotification.h
@@ -5,7 +5,11 @@
 @interface VoIPPushNotification : CDVPlugin <PKPushRegistryDelegate> {
     BOOL foregroundAfterUnlock;
     BOOL appBroughtToFront;
+    BOOL shouldExitApp;
     AVAudioPlayer* audioPlayer;
+    AVAudioPlayer* voipAudioPlayer;
+    AVAudioPlayer* exitAudioPlayer;
+    AVAudioPlayer* ignoreListAudioPlayer;
 }
 
 - (void) init:(CDVInvokedUrlCommand*)command;
