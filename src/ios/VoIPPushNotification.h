c0bcbff06be7383c4d044db0d336be7b076f4d3c
RevillWeb <leon@revillweb.com>
2019-09-20 11:04:22 +0100

    Add stuff to header

--- src/ios/VoIPPushNotification.h
+++ src/ios/VoIPPushNotification.h
@@ -1,7 +1,11 @@
 #import <Cordova/CDV.h>
 #import <PushKit/PushKit.h>
+#import <AVFoundation/AVFoundation.h>
 
-@interface VoIPPushNotification : CDVPlugin <PKPushRegistryDelegate>
+@interface VoIPPushNotification : CDVPlugin <PKPushRegistryDelegate> {
+    AVAudioPlayer* exitAudioPlayer;
+    NSTimer* exitTimer;
+}
 
 - (void)init:(CDVInvokedUrlCommand*)command;
 
